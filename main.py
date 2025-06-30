84ffecbeb0b4b98703bb698f7d6023eccf0214c2
Netanel Berman <nberman@visionrt.com>
2025-06-30 17:01:15 +0100

    second

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+DIGITS = (n for n in range(1, 10))
 s = [[0 for _ in range(9)] for _ in range(9)]
 s = [
     [0, 0, 6, 0, 0, 1, 0, 0, 7],
@@ -10,4 +11,12 @@ s = [
     [1, 0, 0, 0, 0, 6, 7, 0, 5],
     [0, 3, 0, 0, 2, 0, 0, 9, 0],
 ]
+
+def calculate_entropy(s, i, j):
+    # Figure out what it can be from it's row (i)
+    row = s[i]
+    possible_from_row = [n if n not in row else None for n in DIGITS]
+    print(possible_from_row)
+
+calculate_entropy(s, 5, 4)
 print(s)
